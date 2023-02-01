928dba45e0438fc7172c0adc3f33cee34f69b43e
21A91A05E1 <97424503+21A91A05E1@users.noreply.github.com>
2023-02-01 13:46:09 +0530

    import java.util.*; class div {     public static void main(String args[])     {         int d1,d2;         Scanner sc=new Scanner(System.in);         d1=sc.nextInt();         d2=sc.nextInt();         System.out.println(d1/d2);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class div
+{
+    public static void main(String args[])
+    {
+        int d1,d2;
+        Scanner sc=new Scanner(System.in);
+        d1=sc.nextInt();
+        d2=sc.nextInt();
+        System.out.println(d1/d2);
+    }
+}
